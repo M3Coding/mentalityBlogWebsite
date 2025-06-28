b9da7972134b2ea4096dfc706bf4b8c8adee160f
M3Coding <meady2009@gmail.com>
2025-06-27 23:40:07 -0400

    added buttons and functionality and changed css

--- index.js
+++ index.js
@@ -3,23 +3,39 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port =3000;
-let message = [];
+let posts = [];
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(express.json()); // Needed to parse JSON in POST body
 app.get("/", (req, res) => {   
 
     res.render("index.ejs");
 });
 app.get("/blog", (req, res) => {
     
-    res.render("blog.ejs", {message: message});
+    res.render("blog.ejs", {posts: posts});
 });
+//need to add the blog title as h2 and name and also the functionality of delete. 
 app.post("/submit", (req, res) =>{
-    const userInput= message.push(req.body.userPost)
+    var today = new Date()
+    const newPost= {
+        id: Date.now(),
+        content: req.body.userPost,
+        today: today
+
+        };
+        posts.push(newPost);
     
     res.redirect("/blog");
 })
+app.post("/update", (req, res) => {
+  const { id, content } = req.body;
+  const post = posts.find(p => p.id == id);
+  if (post) {
+    post.content = content;
+  }
+  res.sendStatus(200);
+});
 app.get("/about", (req, res) => {
     res.render("about.ejs");
 });
